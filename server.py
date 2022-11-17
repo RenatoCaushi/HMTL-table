4b88efd4dd8d41632a1a0faa441490425b15a6cf
RenatoCaushi <ren.caushi22@gmail.com>
2022-11-17 12:41:00 +0100

    push

---
+++ server.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, redirect, session, request # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+app.secret_key = 'dsasdasdasdasdsdfa'
+
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def staticTable():
+    users = [
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'KB', 'last_name' : 'Tonel'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'}
+]
+    return render_template('index.html', allUsers=users)  # Return the string 'Hello World!' as a response
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
